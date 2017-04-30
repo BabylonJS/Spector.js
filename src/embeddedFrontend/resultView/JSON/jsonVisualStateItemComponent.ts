cfea0e216923b063e72b04db2df726251f401aef
Julian <temechon@gmail.com>
2017-04-30 22:31:07 +0200

    Style updated

--- src/embeddedFrontend/resultView/JSON/jsonVisualStateItemComponent.ts
+++ src/embeddedFrontend/resultView/JSON/jsonVisualStateItemComponent.ts
@@ -8,21 +8,21 @@ namespace SPECTOR.EmbeddedFrontend {
                     const img = document.createElement("img");
                     img.src = encodeURI(imageState.src);
                     divHolder.appendChild(img);
-                    
-                    if (state.Attachments.length > 1) { 
+
+                    if (state.Attachments.length > 1) {
                         const attachment = document.createElement("span");
-                        attachment.innerText = imageState.attachmentName; 
+                        attachment.innerText = imageState.attachmentName;
                         divHolder.appendChild(attachment);
                     }
                 }
             }
             else {
                 const status = document.createElement("span");
-                status.innerText = state.FrameBufferStatus; 
+                status.innerText = state.FrameBufferStatus;
                 divHolder.appendChild(status);
             }
             const fbo = document.createElement("span");
-            fbo.innerText = state.FrameBuffer ? state.FrameBuffer.__SPECTOR_Object_TAG.displayText : "Canvas Frame buffer";
+            fbo.innerText = state.FrameBuffer ? state.FrameBuffer.__SPECTOR_Object_TAG.displayText : "Canvas frame buffer";
             divHolder.appendChild(fbo);
 
             return divHolder;
